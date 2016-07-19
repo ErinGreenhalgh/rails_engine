10e6e10bf6330b17a4cb9c2a1ef66e531282186d
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-07-18 20:27:56 -0600

    add functionality to find all matching records for merchants -user can add params of id, name, created_at, or updated_at to find all the matching merchants -records returned as an array -added corresponding route and controller action'

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :merchants do
-        get '/find', to: 'find#show', defaults: {format: :json}
+        get '/find',     to: 'find#show', defaults: {format: :json}
+        get '/find_all', to: 'find#index', defaults: {format: :json}
       end
     end
   end
