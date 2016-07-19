91e5faf27710765d75a4833e8e85c54960830289
Karina Gonzalez <karinamzalez@gmail.com>
2016-07-18 22:23:38 -0600

    user finds instance of item with any params attribute

---
+++ app/controllers/api/v1/items/find_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::Items::FindController < ApplicationController
+  def show
+    respond_with Item.find_by(item_params)
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :unit_price, :id, :created_at, :updated_at, :merchant_id)
+  end
+end
