9f811b88d5ccbc136a5544a1e27bdcebee1ca23f
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-07-19 06:25:32 -0600

    user can load a single customer -added route -added customers controller namespaced under api::v1

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:show, :index], defaults: {format: :json}
+      resources :customers, only: [:show], defaults: {format: :json}
       resources :items,     only: [:show, :index], defaults: {format: :json}
     end
   end
