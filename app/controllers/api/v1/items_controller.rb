a4163bc74030731926ec2c7e8ca0b336c947b723
Karina Gonzalez <karinamzalez@gmail.com>
2016-07-18 22:55:23 -0600

    fixed strong params in items controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -5,12 +5,12 @@ class Api::V1::ItemsController < ApplicationController
   end
 
   def show
-    respond_with Item.find(params[:id])
+    respond_with Item.find(item_params[:id])
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :unit_price)
+    params.require(:item).permit(:name, :description, :unit_price, :id, :created_at, :updated_at, :merchant_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:show, :index], defaults: {format: :json}
-      resources :items,     only: [:show, :index], defaults: {format: :json}
+      resources :merchants,      only: [:show, :index], defaults: {format: :json}
+      resources :items,          only: [:show, :index], defaults: {format: :json}
+      resources :invoice_items,  only: [:show, :index], defaults: {format: :json}
     end
   end
 
