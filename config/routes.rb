797e5878e306e2f4117dd19b0cbd04f9ce0186e5
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-07-18 20:23:26 -0600

    add functionality for finding a specific merchant -user can hit route to find a specific merchant by id, name, created_at, or updated_at -added namespaced routes and controller structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        get '/find', to: 'find#show', defaults: {format: :json}
+      end
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
       resources :merchants, only: [:show, :index], defaults: {format: :json}
       resources :items,     only: [:show, :index], defaults: {format: :json}
     end
   end
+
 end
