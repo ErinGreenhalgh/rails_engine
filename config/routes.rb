b0670691b44a79da942a191fd52b03131215cd31
Karina Gonzalez <karinamzalez@gmail.com>
2016-07-18 23:40:01 -0600

    added route for nested invoice item find

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,15 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      namespace :invoice_items do
+        get 'find',     to: "find#show",  defaults: {format: :json}
+        get 'find_all', to: "find#index", defaults: {format: :json}
+      end
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
       resources :merchants,      only: [:show, :index], defaults: {format: :json}
       resources :items,          only: [:show, :index], defaults: {format: :json}
       resources :invoice_items,  only: [:show, :index], defaults: {format: :json}
