8ef8ce405c3be8f22139f1615805c21f4a557152
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-07-20 17:48:37 -0600

    create test for total revenue for one merchant -need to uncomment/delete hacky fix for presenting json

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -2,13 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
   scenario "finds the total revenue for a specifc merchant across all transactions" do
-    # merchant = create(:merchant)
-    # invoice1 = create(:invoice)
-    # invoice2 = create(:invoice)
-    # transaction1 = create(:transaction, result: "success", invoice: invoice1)
-    # transaction1 = create(:transaction, result: "failed", invoice: invoice2)
-    # invoice_item = create(:invoice_item)
+    merchant = create(:merchant)
+    invoice1 = create(:invoice, merchant: merchant)
+    invoice2 = create(:invoice, merchant: merchant)
 
-    expect(merchant.total_revenue).to eq 100
+    transaction1 = create(:transaction, result: "success", invoice: invoice1)
+    transaction1 = create(:transaction, result: "failed", invoice: invoice2)
+
+    invoice_item1 = create(:invoice_item, invoice: invoice1)
+    invoice_item2 = create(:invoice_item, invoice: invoice1)
+    invoice_item3 = create(:invoice_item, invoice: invoice2)
+
+    expect(merchant.total_revenue).to eq 200
   end
 end
