e209e05ad0409a52a12456680d63ab06585c3359
Karina Gonzalez <karinamzalez@gmail.com>
2016-07-19 20:31:07 -0600

    fixed overlooked error in routes

--- config/routes.rb
+++ config/routes.rb
@@ -27,8 +27,8 @@ Rails.application.routes.draw do
         get 'find_all', to: "find#index"
       end
       namespace :transactions do
-        get '/find',     to: "find#show", defaults: {format: :json}
-        get '/find_all', to: "find#index", defaults: {format: :json}
+        get '/find',     to: "find#show"
+        get '/find_all', to: "find#index"
       end
     end
   end
