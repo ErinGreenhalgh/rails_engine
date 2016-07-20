2e9cd277172d7e9f7f28f1dee2e4fecfa0d076c4
Karina Gonzalez <karinamzalez@gmail.com>
2016-07-20 13:58:21 -0600

    business relation established * all tests passing

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,10 @@
 class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
+  has_many :customers, through: :invoices
+
+  def customers_pending_invoices
+    self.customers.
+        where("invoices.status = 'pending'")
+  end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :merchants, :invoices,  :customers
+  it "gets list of customers with pending invoices" do
+    merchant = merchants(:one)
+    invoice  = invoices(:one)
+    customer = customers(:one)
+
+    customer_pending_invoice = merchant.customers_pending_invoices.first
+
+    expect(customer_pending_invoice).to eq(customer)
+  end
 end
