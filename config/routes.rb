21a54440c3b2f4c854e7130cb33293db2ef6ca19
Karina Gonzalez <karinamzalez@gmail.com>
2016-07-19 19:53:24 -0600

    refactored routes

--- config/routes.rb
+++ config/routes.rb
@@ -30,12 +30,12 @@ Rails.application.routes.draw do
   end
 
   namespace :api do
-    namespace :v1 do
-      resources :merchants,      only: [:show, :index], defaults: {format: :json}
-      resources :items,          only: [:show, :index], defaults: {format: :json}
-      resources :invoice_items,  only: [:show, :index], defaults: {format: :json}
-      resources :invoices,       only: [:show, :index], defaults: {format: :json}
-      resources :customers, only: [:show, :index], defaults: {format: :json}
+    namespace :v1, defaults: {format: :json} do
+      resources :merchants,      only: [:show, :index]
+      resources :items,          only: [:show, :index]
+      resources :invoice_items,  only: [:show, :index]
+      resources :invoices,       only: [:show, :index]
+      resources :customers,      only: [:show, :index]
     end
   end
 end
