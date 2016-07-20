19899145510e57c751bd3fb3f8bfef18a02088de
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-07-20 16:39:36 -0600

    add AR methods speculated to be correct -for returning total revenue for a merchant across all successful transactions -for returing the total revenue for a merchant given a specific date -for returing the top X merchants by items sold

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,37 @@
 class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
+  has_many :invoice_items, through: :invoices
+  has_many :customers, through: :invoices
+  has_many :transactions, through: :invoices
+
+  def total_revenue
+    successfull_invoice_items.
+    sum("invoice_items.quantity*CAST(invoice_items.unit_price AS integer)")
+  end
+
+  def successful_invoice_items
+    invoices.
+    joins(:transactions, :invoice_items).
+    where.not("transactions.result = 'failed'")
+  end
+
+  def total_revenue_for_date(date)
+    successful_invoice_items.where(created_at: date).
+    sum("invoice_items.quantity*CAST(invoice_items.unit_price AS integer)")
+  end
+
+  def self.most_items(number_of_records)
+    self.joins(invoices: [:transactions, :invoice_items]).
+    where("transactions.result = 'success'").
+    group("merchants.id").
+    order("sum(invoice_items.quantity) DESC").
+    limit(number_of_orders)
+  end
+
+  def self.pending_customers
+    Customer.joins(:invoices, :transactions).
+    where("transactions.result = 'failed'").
+    where("invoices.merchant_id=#{self.id}").distinct
+  end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  scenario "finds the total revenue for a specifc merchant across all transactions" do
+    # merchant = create(:merchant)
+    # invoice1 = create(:invoice)
+    # invoice2 = create(:invoice)
+    # transaction1 = create(:transaction, result: "success", invoice: invoice1)
+    # transaction1 = create(:transaction, result: "failed", invoice: invoice2)
+    # invoice_item = create(:invoice_item)
+
+    expect(merchant.total_revenue).to eq 100
+  end
 end
