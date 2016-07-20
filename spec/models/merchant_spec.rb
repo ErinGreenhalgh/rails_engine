76b0bad8f35ee7ef02cc595d59af1397193812f3
Karina Gonzalez <karinamzalez@gmail.com>
2016-07-20 14:05:29 -0600

    refactored merchant model test

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  fixtures :merchants, :invoices,  :customers
+  fixtures :merchants,  :customers
   it "gets list of customers with pending invoices" do
     merchant = merchants(:one)
-    invoice  = invoices(:one)
     customer = customers(:one)
 
     customer_pending_invoice = merchant.customers_pending_invoices.first
