0c104ea2ed40ca40ba5f08cb0592dc7d311e3632
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-07-20 22:30:28 -0600

    add model test for customers

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
+  fixtures :customers, :transactions, :invoices, :merchants
   scenario "finds the favorite merchant for a customer" do
-  pending
+    customer = customers(:one)
+    merchant = merchants(:one)
 
+    expect(customer.favorite_merchant).to eq merchant
   end
 end
