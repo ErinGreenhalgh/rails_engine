f18335f33a7d9e0fc37c5564b4e348770a712b52
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-07-21 06:43:57 -0600

    add shoulda matchers relationship assertions in all model tests

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ group :development, :test do
   gem 'rspec-rails', '~> 3.4'
   gem 'simplecov'
   gem 'factory_girl_rails'
+  gem 'shoulda-matchers', '~> 3.1'
 end
 
 group :development do
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
   fixtures :customers, :transactions, :invoices, :merchants
+
+  it { should have_many :invoices }
+  it { should have_many :transactions }
+  it { should have_many :merchants }
+  
   scenario "finds the favorite merchant for a customer" do
     customer = customers(:one)
     merchant = merchants(:one)
